125f8e10655b90cca4022f35bc9a8f09f1f82897
louis-25 <20151577@vision.hoseo.edu>
2020-12-02 01:44:43 +0900

    Model

--- django_project/order/models.py
+++ django_project/order/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Dj_Order(models.Model):
+    djuser = models.ForeignKey('user.Dj_User', on_delete=models.CASCADE, verbose_name='사용자') # user의 Dj_User class를 참조
+    product = models.ForeignKey('product.Dj_Product', on_delete=models.CASCADE, verbose_name='상품')
+    quantity = models.IntegerField(verbose_name='수량')
+    register_date = models.DateField(auto_now_add=True, verbose_name='등록날짜')
+    
+    class Meta: # 관리자페이지
+        db_table = 'dj_order'
+        verbose_name = '주문'
+        verbose_name_plural = '주문'
+    
--- django_project/product/models.py
+++ django_project/product/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Dj_Product(models.Model):
+    name = models.CharField(max_length=256, verbose_name='상품명')
+    price = models.IntegerField(verbose_name='상품가격')
+    description = models.TextField(verbose_name='상품설명')
+    stuck = models.IntegerField(verbose_name='재고')
+    register_date = models.DateField(auto_now_add=True, verbose_name='등록날짜')
+
+    class Meta: # 관리자페이지
+        db_table = 'dj_product'
+        verbose_name = '상품'
+        verbose_name_plural = '상품'
